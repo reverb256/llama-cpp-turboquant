0f98b1f1a2e1db589d10af159ddfcd1029bc454c
reverb256 <reverb256@users.noreply.github.com>
2026-04-17 17:42:27 -0500

    wip: quantization work

--- flake.nix
+++ flake.nix
@@ -1,23 +1,23 @@
 {
-  description = "llama.cpp with TurboQuant KV cache compression";
+  description = "llama.cpp with TurboQuant KV cache compression (CUDA, optimized for cluster GPUs)";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    buun-llama-cpp = {
-      url = "github:spiritbuun/buun-llama-cpp";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = { self, nixpkgs, buun-llama-cpp }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        config.cudaSupport = true;
+      };
     in
     {
       packages.${system} = {
         llama-cpp-turboquant = pkgs.callPackage ./package.nix { };
       };
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [ cmake ninja gcc ];
+        packages = with pkgs; [ cmake ninja gcc cudaPackages.cuda_nvcc ];
       };
     };
 }
